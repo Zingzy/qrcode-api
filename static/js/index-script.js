1a90a6304884c00c5be5b766e34ae6c962f4f4bc
Zane <90309290+Zingzy@users.noreply.github.com>
2023-12-13 15:12:40 +0530

    Bug-Fixes

--- static/js/index-script.js
+++ static/js/index-script.js
@@ -52,11 +52,11 @@ function generateQRCode() {
     var apiUrl;
 
     if (qrType === "simple") {
-        apiUrl = `https://qrcode-api-ten.vercel.app/classic?text=${encodeURIComponent(
+        apiUrl = `https://qr.spoo.me/classic?text=${encodeURIComponent(
             userInput,
         )}&fill=${fillColor}&back=${bgColor}`;
     } else {
-        apiUrl = `https://qrcode-api-ten.vercel.app/gradient?text=${encodeURIComponent(
+        apiUrl = `https://qr.spoo.me/gradient?text=${encodeURIComponent(
             userInput,
         )}&gradient1=${gradientColor}&gradient2=${gradient2Color}`;
     }
